1b2343c1f9d75c369ae178345959fbdd0caf5ef8
PaulaVirBurgos <paulavburgos@gmail.com>
2022-03-09 09:16:15 -0300

    inicio repo

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require ('express'); 
+const app = express(); 
+const path = require('path'); 
+const port = 3030; 
+
+app.use(express.static(path.join('public')))
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+}); 
+app.get('/about', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+}); 
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'notfound.html'))
+}); 
+app.listen(port, ()=>{
+    console.log(`Server running on port http://localhost:${port}`);
+})
